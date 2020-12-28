b6598c5eb5abb21c9d6ee213a5a21833dda9c1e6
soobin <nhsb00@hotmail.com>
2020-12-28 14:52:21 -0500

    arguments exercise finished

--- argumentsExercise.js
+++ argumentsExercise.js
@@ -16,13 +16,16 @@ function sum2 (...nums) {
   };
 
   return sum;
-}
+};
 
 Function.prototype.myBind = function (ctx) {
   const fn = this;
-  const bindArgs = Array.from(arguments).slice(1)
-  return fn.apply(ctx, bindArgs.concat())
-}
+  const bindArgs = Array.from(arguments).slice(1);
+  return function boundFn () {
+    const callArgs = Array.from(arguments);
+    return fn.apply(ctx, bindArgs.concat(callArgs));
+  };
+};
 
 // tester = function () {
 //   console.log(this);
@@ -30,3 +33,58 @@ Function.prototype.myBind = function (ctx) {
 // }
 
 // a = tester.myBind({name: "John"});
+
+Function.prototype.myBind2 = function (ctx, ...bindArgs) {
+  return function boundFn(...callArgs) {
+    return this.apply(ctx, bindArgs.concat(callArgs));
+  };
+};
+
+
+function curriedSum(numArgs) {
+  const numbers = [];
+    function _curriedSum(num) {
+      numbers.push(num);
+      if (numbers.length === numArgs) {
+        debugger
+        let total = 0;
+
+        numbers.forEach((n) => { total += n; });
+        return total 
+      } else {
+        debugger
+        // return
+        return _curriedSum;
+      }
+    }
+    debugger
+    return _curriedSum;
+}
+
+Function.prototype.curry = function(numArgs) {
+  const args = [];
+  const fn = this;
+  function _curriedFn(arg) {
+    args.push(arg);
+    if (args.length === numArgs) {
+      return fn.apply(null, args);
+    } else {
+      return _curriedFn;
+    }
+  }
+  return _curriedFn;
+}
+
+Function.prototype.curry1 = function (numArgs) {
+  const args = [];
+  const fn = this;
+  function _curriedFn(arg) {
+    args.push(arg);
+    if (args.length === numArgs) {
+      return fn(...args);
+    } else {
+      return _curriedFn;
+    }
+  }
+  return _curriedFn;
+}
