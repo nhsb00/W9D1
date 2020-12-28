cf00970a63512308fddc5315530e4c0257dface7
Paul Ramirez <pramirez7230@bths.edu>
2020-12-28 15:42:03 -0500

    Finish inherits exercise

--- inherits_exercise.js
+++ inherits_exercise.js
@@ -1,30 +1,49 @@
 Function.prototype.inherits = function (SuperClass) {
-    function Surrogate () {}
+    function Surrogate () {};
     Surrogate.prototype = SuperClass.prototype;
     this.prototype = new Surrogate();
     this.prototype.constructor = this;
-}
+};
 
-function MovingObject(name) {
-    this.name = name;
-    function move () {
-        console.log("moving")
-    }
-}
-
+function MovingObject() {};
+  
 function Ship(name) {
-    MovingObject.call(this, name);
-    function float () {
-        console.log("floating");
-    }
-}
+  this.name = name;
+};
 Ship.inherits(MovingObject);
 
 function Asteroid(name) {
-    MovingObject.call(this, name);
-    function fall() {
-        console.log("fall");
-    }
-}
+  this.name = name;
+};
 Asteroid.inherits(MovingObject);
+  
+let titanic = new Ship("titanic");
+let ast = new Asteroid("ast");
 
+MovingObject.prototype.move = function () {
+  console.log("moving");
+};
+
+Ship.prototype.float = function () {
+  console.log("floating");
+};
+
+Asteroid.prototype.fall = function () {
+  console.log("fall");
+};
+
+titanic.float();
+ast.fall();
+
+titanic.fall();
+ast.float();
+
+titanic.move();
+ast.move();
+
+// Function.prototype.inherits using Object.create
+
+Function.prototype.inherits = function (SuperClass) {
+  this.prototype = Object.create(SuperClass.prototype);
+  this.prototype.constructor = this;
+};
